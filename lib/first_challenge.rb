10b0bcfd5eecb1ed7edafb44c3ac02aa6a21c3ce
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-07 23:41:04 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -12,11 +12,12 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
   #your code here
 
-
-  #remember to return your newly altered contacts hash!
+  contacts['Freddy Mercury'].collect do |attribute, info|
+    if attribute ='favorite_icecream_flavors'
+      info.delete('strawberry')
+    end
+  end
   contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,9 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
   #code your solution here!
+  groceries.values.flatten
 
-  
+
 
 end
